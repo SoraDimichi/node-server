9acf827e1926100b3cec237c564497ac2b97783e
Dmitry Sorokin <soradimichi@gmail.com>
2024-01-21 16:49:55 +0600

    linter fixes

--- transport/fastify/ws.ts
+++ transport/fastify/ws.ts
@@ -14,42 +14,42 @@ const startServer = (
 ): void => {
   const fastify = Fastify();
 
-  fastify.register(websocket);
-  fastify.register(async function (fastify) {
+  void fastify.register(websocket);
+  void fastify.register(async function (fastify) {
     fastify.get("/*", { websocket: true }, (connection, req) => {
-      // bound to fastify server
-
       const ip = (req.socket.address() as AddressInfo).address;
-      connection.socket.on("message", async (message, req) => {
-        const obj = JSON.parse(message.toString());
-        const { name, method, args = [] } = obj;
-        const entity = routing[name];
-        if (entity == null) {
-          connection.socket.send('"Not found"', { binary: false });
-          return;
-        }
-        const handler = entity[method];
-        if (handler == null) {
-          connection.socket.send('"Not found"', { binary: false });
-          return;
-        }
-        const json = JSON.stringify(args);
-        const parameters = json.substring(1, json.length - 1);
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-        logger.log(`${ip} ${name}.${method}(${parameters})`);
-        try {
-          const result = await handler(...args);
-          connection.socket.send(JSON.stringify(result as string), {
-            binary: false,
-          });
-        } catch (err) {
-          logger.error(err);
-          connection.socket.send('"Server error"', { binary: false });
-        }
+      connection.socket.on("message", (message, req) => {
+        void (async () => {
+          const obj = JSON.parse(message.toString()); // eslint-disable-line @typescript-eslint/no-base-to-string
+          const { name, method, args = [] } = obj;
+          const entity = routing[name];
+          if (entity == null) {
+            connection.socket.send('"Not found"', { binary: false });
+            return;
+          }
+          const handler = entity[method];
+          if (handler == null) {
+            connection.socket.send('"Not found"', { binary: false });
+            return;
+          }
+          const json = JSON.stringify(args);
+          const parameters = json.substring(1, json.length - 1);
+          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+          logger.log(`${ip} ${name}.${method}(${parameters})`);
+          try {
+            const result = await handler(...args);
+            connection.socket.send(JSON.stringify(result as string), {
+              binary: false,
+            });
+          } catch (err) {
+            logger.error(err);
+            connection.socket.send('"Server error"', { binary: false });
+          }
+        })();
       });
     });
   });
-  fastify.listen({ port, host: "0.0.0.0" });
+  void fastify.listen({ port, host: "0.0.0.0" });
 
   logger.log(`API on port ${port}`);
 };
